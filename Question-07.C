94d042100ad5e83936655ba070cc4c5dd2c620d8
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-07-01 18:15:09 +0530

    Create Question-07.C

---
+++ Question-07.C
@@ -0,0 +1,42 @@
+// Write a program in C to print or display the lower triangular of a given matrix.
+
+#include<stdio.h>
+
+int main ()
+{
+        int m,n,i,j ;
+
+        printf("Enter size ( m x n ) : ");
+        scanf("%d%d",&m,&n);
+
+        int a[m][n] ;
+
+        printf("Enter values of a[] :\n");
+        for(i=0 ; i<m ; i++)
+                for(j=0 ; j<n ; j++)
+                        scanf("%d",&a[i][j] );
+
+
+        for(i=0 ; i<m ; i++)
+        {
+                for(j=0 ; j<n ; j++)
+                {
+                        if(i > j)
+                                a[i][j] = 0 ;
+                }
+        }
+
+
+        printf("the lower triangular :\n");
+        for(i=0 ; i<m ; i++)
+        {
+                for(j=0 ; j<n ; j++)
+                        printf("%d ",a[i][j]);
+
+                printf("\n");
+
+        }
+
+        return 0 ;
+
+}
